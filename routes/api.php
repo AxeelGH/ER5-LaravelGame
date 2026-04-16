e3d2efb1b39016c16931c11b926909aa20de9e5e
AxeelGH <axelpg2004@gmail.com>
2026-04-16 10:47:02 +0200

    Feat: added login

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', function (Request $request){
+    if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+        return response()->json(['ok'=>true]);
+    }
+    return response()->json(['ok'=>false],401);
+});
